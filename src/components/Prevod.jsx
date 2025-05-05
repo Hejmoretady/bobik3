9259db941a2fb561c23da5e779ff8f1275d849a9
kokot <adam.hinte@gmail.com>
2025-05-05 12:39:33 +0200

    změna

---
+++ src/components/Prevod.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+export const Prevod = () => {
+    const [cislo, setCislo] = useState(0);
+}
+
+const Prevod = () => {
+    return(
+        <div>
+            <h1>
+                Převod EUR do jiné měny
+            </h1>
+            <div>
+                <input
+                    type ="number"
+                    value = {text}
+                    onChange={(e) => setText(e.target.value)}
+                    style={{padding: '5px'}}
+                />
+                <select></select>
+                <button>Převeď</button>
+            </div>
+        </div>
+    )
+}
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA({
+  plugins: [react(), VitePWA({
     registerType: 'prompt',
     injectRegister: false,
 
