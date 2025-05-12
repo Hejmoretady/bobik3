e78c8c590fc1816682caefdbffb6687ed1ee1927
kokot <adam.hinte@gmail.com>
2025-05-12 12:17:04 +0200

    magor

--- src/components/Prevod.jsx
+++ src/components/Prevod.jsx
@@ -1,7 +1,29 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 export const Prevod = () => {
     const [cislo, setCislo] = useState(0);
+    const [kurzy, setKurzy] = useState(undefined);
+    const [vysledek, setVysledek] = useState(0);
+    const [vybrany, setVybrany] = useState(0);
+
+    const getKurzy = async () => {
+        try {
+            const response = await fetch("https://api.frankfurter.dev/v1/latest?base=EUR");
+            const data = await response.json();
+            setKurzy(data.rates);
+            console.log(kurzy);
+        } catch(error) {
+            console.log(error);
+        }
+    };
+    
+    useEffect(() => {
+        getKurzy();
+    }, []);
+
+    const vypocitej = () => {
+        setVysledek(vybrany * cislo);
+    }
 
     return(
         <div>
@@ -15,9 +37,16 @@ export const Prevod = () => {
                     onChange={(e) => setCislo(e.target.value)}
                     style={{ padding: '5px' }}
                 />
-                <select></select>
-                <button>Převeď</button>
+                <select onChange={(e) => setVybrany(e.target.value)}>
+                    {kurzy &&
+                        Object.entries(kurzy).map(([key, value]) => (
+                            <option value={value} key={key}>{key}</option>
+                        ))
+                    }
+                </select>
+                <button onClick={() => vypocitej()}>Převeď</button>
             </div>
+            <h3>{vysledek}</h3>
         </div>
     )
 }
